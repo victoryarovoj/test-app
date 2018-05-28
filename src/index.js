cdbe53e500123ad6b460823546da872003f14bf2
victoryarovoj <victoryarovoj@gmail.com>
2018-05-28 14:49:35 +0300

    Fixed index

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 
+const initialState = {
+	uuid: [
+		'first item',
+		'second item'
+	]
+};
 
-// function userInfo(state = initialState, action) {
+function userInfo(state = initialState, action) {
 
-// 	return {
-// 		...state,
-// 		uuid: [...state.uuid, action.payload]
-// 	};
+	return {
+		...state,
+		uuid: [...state.uuid, action.payload]
+	};
 
-// 	console.log(action);
-// 	return state;
-// }
+	console.log(action);
+	return state;
+}
 
-// const store = createStore(userInfo);
+const store = createStore(userInfo);
 
 ReactDom.render(
 	<Provider store={store}>
