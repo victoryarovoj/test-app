55508174ba048a1474fdf0645846f02d23a94d36
victoryarovoj <victoryarovoj@gmail.com>
2018-05-28 13:49:50 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,6 @@ import Header from './Header';
 import Uploader from './Uploader';
 
 class App extends Component {
-	addTrack() {
-		console.log('addTrack', this.trackInput.value);
-		this.props.onAddTrack(this.trackInput.value);
-		this.trackInput.value = '';
-	}
 	render() {
 		return (
 			<div align="center">
--- src/index.js
+++ src/index.js
@@ -4,25 +4,19 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 
-const initialState = {
-	uuid: [
-		'first item',
-		'second item'
-	]
-};
 
-function userInfo(state = initialState, action) {
+// function userInfo(state = initialState, action) {
 
-	return {
-		...state,
-		uuid: [...state.uuid, action.payload]
-	};
+// 	return {
+// 		...state,
+// 		uuid: [...state.uuid, action.payload]
+// 	};
 
-	console.log(action);
-	return state;
-}
+// 	console.log(action);
+// 	return state;
+// }
 
-const store = createStore(userInfo);
+// const store = createStore(userInfo);
 
 ReactDom.render(
 	<Provider store={store}>
@@ -30,40 +24,3 @@ ReactDom.render(
 	</Provider>, 
 	document.getElementById('root')
 );
-
-// import { createStore } from 'redux';
-
-// function playList(state = [], action) {
-// 	if (action.type === 'ADD_TRACK') {
-// 		return [
-// 			...state,
-// 			action.payload
-// 		];
-// 	}
-// 	console.log(action);
-// 	return state;
-// }
-
-// const store = createStore(playList);
-
-// console.log(store.getState());
-
-// store.subscribe(() => {
-// 	console.log('subscribe', store.getState());
-// 	const list = document.querySelectorAll('.list')[0];
-// 	list.innerHTML = '';
-// 	document.querySelectorAll('.trackInput')[0].value = '';
-// 	store.getState().forEach(track => {
-// 		const li = document.createElement('li');
-// 		li.textContent = track;
-// 		list.appendChild(li);
-// 	});
-// });
-
-
-// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-// addTrackBtn.addEventListener('click', () => {
-// 	const trackName = document.querySelectorAll('.trackInput')[0].value;
-// 	console.log('trackName', trackName);
-// 	store.dispatch({type: 'ADD_TRACK', payload: trackName});
-// });
